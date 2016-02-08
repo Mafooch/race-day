d32449d0676eb008758e0a8d2f9c20fbfa8fdbc5
Mafooch <joshuamaffuccio@gmail.com>
2016-02-08 17:34:51 -0500

    Add all class method for querying to Racer model

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -11,4 +11,25 @@ class Racer
     self.mongo_client["racers"]
   end
 
+  def self.all prototype = {}, sort = { number: 1 }, skip = 0, limit = nil
+    tmp = {}
+    sort.each do |k, v|
+      k = k.to_sym
+      tmp[k] = v if [:number, :first_name, :last_name, :gender, :group, :secs].include?(k)
+    end
+    sort = tmp
+
+    #convert to keys and then eliminate any properties not of interest
+    prototype = prototype.symbolize_keys.slice(:number, :first_name, :last_name, :gender, :group, :secs) unless prototype.nil?
+
+    Rails.logger.debug { "getting all racers, prototype=#{prototype}, sort=#{sort}, skip=#{skip}, limit=#{limit}" }
+
+    result = collection.find(prototype)
+             .sort(sort)
+             .skip(skip)
+    result = result.limit limit unless limit.nil?
+
+    return result
+  end
+
 end
